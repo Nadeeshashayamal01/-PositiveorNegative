5783bf89a11ee8d1acef528ff71179f0f5a2452b
DhanujiIresha <101620231+DhanujiIresha@users.noreply.github.com>
2022-10-12 20:48:01 +0530

    Update posneg.java

--- posneg.java
+++ posneg.java
@@ -6,7 +6,7 @@ public class posneg {
 
         // true if number is less than 0
         if (number < 0.0)
-            System.out.println(num + " is  negative number.");
+            System.out.println(number + " is  negative number.");
 
         // true if number is greater than 0
         else if ( number > 0.0)
